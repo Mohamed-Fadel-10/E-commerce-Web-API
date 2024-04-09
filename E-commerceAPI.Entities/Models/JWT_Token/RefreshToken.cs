cd416240f7a581848adaa15c3a5b58e3de403ac5
Mohamed Fadel <fadelmohamed445@gmail.com>
2024-04-09 17:44:43 +0200

    Add Prohect Files

---
+++ E-commerceAPI.Entities/Models/JWT_Token/RefreshToken.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_commerceAPI.Entities.Models.JWT_Token
+{
+    [Owned]
+    public  class RefreshToken
+    {
+        public string Token { get; set; }
+        public DateTime ExpiresOn { get; set; }
+        public bool IsExpired => DateTime.UtcNow >= ExpiresOn;
+        public DateTime CreatedOn { get; set; }
+        public DateTime? RevokeOn { get; set; }
+        public bool IsActive => RevokeOn == null && !IsExpired;
+
+    }
+}
